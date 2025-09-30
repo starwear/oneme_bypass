fbd64eba37c9e5df45af779582c7785eefcb798a
Alexey Polyakov <starwear3000@ya.ru>
2025-09-30 23:25:42 +0300

    Передача юзер-агента для большей надежности

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ from websockets.asyncio.client import connect
 # Адрес оригинального вебссокета маха
 ONEME_WS_URL = "wss://ws-api.oneme.ru/websocket"
 
+# Юзер-агент для большей надежности
+HEADER_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0"
+
 async def repeater(websocket):
     # Устанавливаем в фоне подключение с оригинальным вебсокетом
-    oneme_ws = await connect(ONEME_WS_URL, origin="https://web.max.ru")
+    oneme_ws = await connect(
+        uri=ONEME_WS_URL,
+        origin="https://web.max.ru",
+        user_agent_header=HEADER_USER_AGENT
+    )
     
     async def forward_to_oneme():
         """Функция перенаправки пакетов в оригинальный вебсокет макса"""
         async for message in websocket:
+            print(">>> {}".format(message))
             await oneme_ws.send(message)
 
     async def forward_to_client():
         """Функция перенаправки пакетов клиенту"""
         async for message in oneme_ws:
+            print("<<< {}".format(message))
             await websocket.send(message)
 
     await asyncio.gather(forward_to_oneme(), forward_to_client())
